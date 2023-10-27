96f5e7ef22ebae931c7a7ace920d54a14f3e4b2f
quangle100199 <62121215+quangle100199@users.noreply.github.com>
2023-10-26 19:23:04 -0500

    Update DeltaDebugger.java
    1. I've merged the methods to compile and run Java files into a single compileAndRun method.
    2. A timeout of 10 seconds has been added to the java command. If the program doesn't finish executing within this time, it will be terminated, and the result will be UNRESOLVED.
    3. The split method has been renamed to getDifferences to avoid confusion. The method to divide changes has been renamed to divideChanges.

--- DeltaDebugger.java
+++ DeltaDebugger.java
@@ -9,8 +9,8 @@ public class DeltaDebugger {
   static final int UNRESOLVED = 2;
 
 
-  public static List<Integer> dd(List<Integer> changes, boolean baselinePasses, boolean allChangesFail) {
-    return ddRecursive(changes, new ArrayList<Integer>());
+  public static List<Integer> dd(List<Integer> changes) {
+    return ddRecursive(changes, new ArrayList<>());
   }
   public static List<Integer> ddRecursive(List<Integer> changes, List<Integer> remaining) {
     int n = changes.size();
@@ -20,7 +20,7 @@ public class DeltaDebugger {
 
     List<Integer> c1 = new ArrayList<>();
     List<Integer> c2 = new ArrayList<>();
-    split(changes, c1, c2);
+    divideChanges(changes, c1, c2);
 
     if (test(join(c1, remaining)) == FAIL) {
       return ddRecursive(c1, remaining);
@@ -39,32 +39,39 @@ public class DeltaDebugger {
     // Create process builder
     ProcessBuilder pb = new ProcessBuilder("diff", "-U", "0", fileName1, fileName2);
     Process process;
-    int errCode;
+    List<String> output = new ArrayList<>();
     // Start and wait for process to finish
-    try{
+    try {
       process = pb.start();
-      errCode = process.waitFor();
-
-      // Check if error occurred
+      try (BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+          String line;
+          while ((line = br.readLine()) != null) {
+              output.add(line);
+          }
+      }
+      int errCode = process.waitFor();
       if (errCode == 0) {
-        System.out.println("No differences found");
-        return new ArrayList<>();
+          System.out.println("No differences found");
+          return new ArrayList<>();
       }
+    } catch (Exception e) {
+        System.out.println("Error during diff: " + e.getMessage());
+    }
+    return output;
+  }
 
-      // Read output
-      BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      String line;
-      List<String> output = new ArrayList<>();
-      while ((line = br.readLine()) != null) {
-        output.add(line);
-      }
-      // Return output
-      return output;
-    }catch(Exception e){
-      System.out.println("Error");
-      return new ArrayList<>();
+  //new 
+  public static void divideChanges(List<Integer> changes, List<Integer> c1, List<Integer> c2) {
+    int halfSize = changes.size() / 2;
+    for (int i = 0; i < changes.size(); i++) {
+        if (i < halfSize) {
+            c1.add(changes.get(i));
+        } else {
+            c2.add(changes.get(i));
+        }
     }
   }
+
 //  public static List<Integer> split(List<Integer> changes, List<Integer> c1, List<Integer> c2) {
 //    Random rand = new Random();
 //    for (Integer change : changes) {
@@ -77,34 +84,54 @@ public class DeltaDebugger {
 //    return c1;
 //  }
   public static List<Integer> join(List<Integer> l1, List<Integer> l2) {
-    List<Integer> joined = new ArrayList<>();
-    joined.addAll(l1);
+    List<Integer> joined = new ArrayList<>(l1);
     joined.addAll(l2);
     return joined;
   }
 
   //runs file name to see if it passes, fails, or returns unresolved.
   // Not quite sure what would constitute unresolved atm
-  public static int test(String fileName) {
-    ProcessBuilder pb;
-    Process p;
-    int error;
-    try{
-      //compile the program
-      pb = new ProcessBuilder("javac", fileName);
-      p = pb.start();
-      p.waitFor();
+  public static int test(List<Integer> changes) {
+    String fileName = "YourFileNameHere.java"; // Replace this with a method to get the file name based on changes or input
+    int result = compileAndRun(fileName);
+    return result;
+  }
 
-      //run the program
-      pb = new ProcessBuilder("java", fileName);
-      p = pb.start();
-      p.waitFor(); //throws exception if an error occurs
+  public static int compileAndRun(String fileName) {
+    ProcessBuilder pb;
+    int resultCode = UNRESOLVED;
+    try {
+        // Compile the program
+        pb = new ProcessBuilder("javac", fileName);
+        Process compileProcess = pb.start();
+        int compileExitCode = compileProcess.waitFor();
+        if (compileExitCode != 0) {
+            return FAIL; // Compilation failed
+        }
+
+        // Run the program and set a timeout
+        pb = new ProcessBuilder("java", fileName.replace(".java", ""));
+        Process runProcess = pb.start();
+        boolean completed = runProcess.waitFor(10, java.util.concurrent.TimeUnit.SECONDS); // 10 seconds timeout
+
+        if (completed) {
+            if (runProcess.exitValue() == 0) {
+                resultCode = PASS;
+            } else {
+                resultCode = FAIL;
+            }
+        } else {
+            runProcess.destroy();
+        }
+    } catch (Exception e) {
+        System.out.println("Error during compile or run: " + e.getMessage());
+        resultCode = FAIL;
     }
-    catch(Exception e){
-      return FAIL;
-    }
-    return PASS;
-    //return UNRESOLVED???
+    return resultCode;
+  }
+
+  public static void main(String[] args) {
+    // for testing or driver code
   }
 
   //  public static int test(List<Integer> changes) {
